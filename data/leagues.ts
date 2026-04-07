9db20ffc6df0ca527dedf9aaa71ecf9cad5ae015
chuheejjang-wq <chuheejjang@gmail.com>
2026-04-07 22:04:11 +0900

    using logo git url

--- data/leagues.ts
+++ data/leagues.ts
@@ -5,42 +5,42 @@ export const leagues: League[] = [
     code: 'KBO',
     name: 'KBO',
     country: 'KR',
-    imageUrl: '/logos/leagues/kbo.svg',
+    imageUrl: 'https://raw.githubusercontent.com/chuheejjang-wq/sportsgo/refs/heads/master/public/logos/leagues/kbo.svg',
     officialUrl: 'https://www.koreabaseball.com/'
   },
   {
     code: 'KLEAGUE1',
     name: 'K League 1',
     country: 'KR',
-    imageUrl: '/logos/leagues/kleague1.svg',
+    imageUrl: 'https://raw.githubusercontent.com/chuheejjang-wq/sportsgo/refs/heads/master/public/logos/leagues/kleague1.svg',
     officialUrl: 'https://www.kleague.com/'
   },
   {
     code: 'KLEAGUE2',
     name: 'K League 2',
     country: 'KR',
-    imageUrl: '/logos/leagues/kleague2.svg',
+    imageUrl: 'https://raw.githubusercontent.com/chuheejjang-wq/sportsgo/refs/heads/master/public/logos/leagues/kleague2.svg',
     officialUrl: 'https://www.kleague.com/'
   },
   {
     code: 'NPB',
     name: 'NPB',
     country: 'JP',
-    imageUrl: '/logos/leagues/npb.svg',
+    imageUrl: 'https://raw.githubusercontent.com/chuheejjang-wq/sportsgo/refs/heads/master/public/logos/leagues/npb.svg',
     officialUrl: 'https://npb.jp/'
   },
   {
     code: 'J1',
     name: 'J1 League',
     country: 'JP',
-    imageUrl: '/logos/leagues/j1.svg',
+    imageUrl: 'https://raw.githubusercontent.com/chuheejjang-wq/sportsgo/refs/heads/master/public/logos/leagues/j1.svg',
     officialUrl: 'https://www.jleague.co/'
   },
   {
     code: 'J2',
     name: 'J2 League',
     country: 'JP',
-    imageUrl: '/logos/leagues/j2.svg',
+    imageUrl: 'https://raw.githubusercontent.com/chuheejjang-wq/sportsgo/refs/heads/master/public/logos/leagues/j2.svg',
     officialUrl: 'https://www.jleague.co/'
   }
 ];
